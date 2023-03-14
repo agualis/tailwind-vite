b427dd34c3af6554942101f5cf01b774ee748366
Alberto Gualis <agualis@gmail.com>
2023-03-14 12:28:04 +0100

    Use flex-col

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -3,9 +3,9 @@ import MyButton from "./MyButton.vue";
 </script>
 
 <template>
-  <div class="card flex space-x-4">
+  <div class="card flex flex-col space-y-4">
     <MyButton color="blue">Blue</MyButton>
-    <MyButton size="sm" color="red">Small red</MyButton>
+    <MyButton size="xs" color="red">Small red</MyButton>
     <MyButton color="gradient">Gradient</MyButton>
     <MyButton color="gradient-pink-yellow">Gradient-pink-yellow</MyButton>
     <MyButton disabled>Disabled</MyButton>
